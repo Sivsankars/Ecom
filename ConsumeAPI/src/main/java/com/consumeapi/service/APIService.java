8a8b4a3f2a3c8f10f2b67fc5e4d936cd72a39376
arunsnair135@gmail.com <arunsnair135@gmail.com>
2023-04-06 12:28:12 +0530

    added the api services

---
+++ ConsumeAPI/src/main/java/com/consumeapi/service/APIService.java
@@ -0,0 +1,33 @@
+package com.consumeapi.service;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class APIService {
+	
+	private final RestTemplate restTemplate;
+	
+	@Autowired
+	public APIService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	public Object consumeAPI() {		
+		String userName="arunsnair135";
+		try {
+			return restTemplate.getForObject(
+					new URL("https://api.github.com/users/"+userName+"/repos").toURI(), 
+					Object.class);
+		} catch (RestClientException | MalformedURLException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
